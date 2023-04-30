d3058e359e0c5255c0adf7467789ddf5ad127ce5
Akshat Rai <raiakshat007@gmail.com>
2023-04-30 11:40:00 +0530

    uploading changes

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'chatapp UI',
+      title: 'Hommie Chat',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
         appBarTheme: const AppBarTheme(
